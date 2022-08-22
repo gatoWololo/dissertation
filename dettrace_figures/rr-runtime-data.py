5a5f2c0d81225d271b7328148fc23c4c865c4c02
Omar Navarro Leija <leija.this@gmail.com>
2022-08-22 15:42:46 -0400

    Initial Overleaf Import

---
+++ dettrace_figures/rr-runtime-data.py
@@ -0,0 +1,104 @@
+rrRawTimeOutput = """cherrypy3 87.12user 25.19system 5:05.05elapsed 36%CPU (0avgtext+0avgdata 113640maxresident)k
+gnome-audio 11.71user 3.47system 0:14.08elapsed 107%CPU (0avgtext+0avgdata 97564maxresident)k
+apt-listchanges 19.58user 4.86system 0:22.34elapsed 109%CPU (0avgtext+0avgdata 97652maxresident)k
+testresources 35.34user 10.19system 0:42.60elapsed 106%CPU (0avgtext+0avgdata 103420maxresident)k
+muttprofile 8.46user 2.63system 0:10.70elapsed 103%CPU (0avgtext+0avgdata 93988maxresident)k
+libhtml-stream-perl 12.73user 4.17system 0:16.22elapsed 104%CPU (0avgtext+0avgdata 95972maxresident)k
+python-xmlrunner 14.23user 4.17system 0:17.27elapsed 106%CPU (0avgtext+0avgdata 104024maxresident)k
+libpalm-perl 15.46user 4.68system 0:19.60elapsed 102%CPU (0avgtext+0avgdata 95728maxresident)k
+taglog 9.05user 2.90system 0:11.66elapsed 102%CPU (0avgtext+0avgdata 95824maxresident)k
+libmath-sparsematrix-perl 8.63user 2.61system 0:10.74elapsed 104%CPU (0avgtext+0avgdata 93344maxresident)k
+librose-object-perl 14.93user 4.54system 0:19.19elapsed 101%CPU (0avgtext+0avgdata 97156maxresident)k
+nagios-images 34.91user 11.19system 0:44.06elapsed 104%CPU (0avgtext+0avgdata 98872maxresident)k
+libclass-gomor-perl 11.68user 3.64system 0:15.21elapsed 100%CPU (0avgtext+0avgdata 96072maxresident)k
+libhtml-encoding-perl 12.47user 3.90system 0:15.66elapsed 104%CPU (0avgtext+0avgdata 97632maxresident)k
+lunch 20.34user 5.76system 0:24.50elapsed 106%CPU (0avgtext+0avgdata 104564maxresident)k
+fonts-lohit-taml 9.22user 2.78system 0:11.83elapsed 101%CPU (0avgtext+0avgdata 96300maxresident)k
+libnet-ipv6addr-perl 12.38user 3.79system 0:15.50elapsed 104%CPU (0avgtext+0avgdata 95116maxresident)k
+libpetal-perl 24.72user 7.32system 0:33.17elapsed 96%CPU (0avgtext+0avgdata 101280maxresident)k
+libpoe-filter-stomp-perl 12.80user 4.03system 0:16.44elapsed 102%CPU (0avgtext+0avgdata 95116maxresident)k
+when 10.16user 2.92system 0:12.62elapsed 103%CPU (0avgtext+0avgdata 95736maxresident)k
+r-cran-rocr 22.64user 5.18system 0:22.27elapsed 124%CPU (0avgtext+0avgdata 102328maxresident)k
+libmath-base36-perl 14.12user 4.31system 0:17.55elapsed 105%CPU (0avgtext+0avgdata 94080maxresident)k
+m17n-docs 79.78user 26.04system 1:40.36elapsed 105%CPU (0avgtext+0avgdata 100476maxresident)k
+flufl.enum 45.22user 12.84system 0:54.21elapsed 107%CPU (0avgtext+0avgdata 114560maxresident)k
+metainit 11.67user 3.71system 0:14.46elapsed 106%CPU (0avgtext+0avgdata 98488maxresident)k
+aspell-el 13.77user 4.15system 0:17.28elapsed 103%CPU (0avgtext+0avgdata 97520maxresident)k
+tor-arm 25.53user 7.33system 0:31.29elapsed 105%CPU (0avgtext+0avgdata 105624maxresident)k
+nautilus-scripts-manager 19.30user 5.57system 0:23.17elapsed 107%CPU (0avgtext+0avgdata 105280maxresident)k
+log4cpp-doc 76.99user 24.68system 1:33.88elapsed 108%CPU (0avgtext+0avgdata 100416maxresident)k
+libnet-subnets-perl 12.58user 3.98system 0:16.00elapsed 103%CPU (0avgtext+0avgdata 95848maxresident)k
+libpod-simple-wiki-perl 21.08user 6.33system 0:27.32elapsed 100%CPU (0avgtext+0avgdata 99500maxresident)k
+flask 63.76user 18.70system 1:37.57elapsed 84%CPU (0avgtext+0avgdata 113240maxresident)k
+django-notification 32.39user 9.17system 0:39.61elapsed 104%CPU (0avgtext+0avgdata 110304maxresident)k
+fts 48.49user 14.51system 0:58.61elapsed 107%CPU (0avgtext+0avgdata 107788maxresident)k"""
+#ocamlweb 21.58user 6.73system 0:27.57elapsed 102%CPU (0avgtext+0avgdata 100108maxresident)k"""
+
+
+nativeRawTimeOutput = """cherrypy3 19.90user 2.76system 1:31.26elapsed 24%CPU (0avgtext+0avgdata 119568maxresident)k
+gnome-audio 1.60user 0.19system 0:01.82elapsed 98%CPU (0avgtext+0avgdata 42540maxresident)k
+apt-listchanges 3.55user 0.59system 0:04.18elapsed 99%CPU (0avgtext+0avgdata 47208maxresident)k
+testresources 3.67user 0.71system 0:04.50elapsed 97%CPU (0avgtext+0avgdata 42312maxresident)k
+muttprofile 1.21user 0.29system 0:01.52elapsed 98%CPU (0avgtext+0avgdata 22500maxresident)k
+libhtml-stream-perl 1.79user 0.34system 0:02.17elapsed 98%CPU (0avgtext+0avgdata 22596maxresident)k
+python-xmlrunner 1.78user 0.33system 0:02.17elapsed 97%CPU (0avgtext+0avgdata 23000maxresident)k
+libpalm-perl 2.26user 0.37system 0:02.68elapsed 98%CPU (0avgtext+0avgdata 22520maxresident)k
+taglog 1.42user 0.23system 0:01.69elapsed 97%CPU (0avgtext+0avgdata 26236maxresident)k
+libmath-sparsematrix-perl 1.69user 0.26system 0:01.98elapsed 98%CPU (0avgtext+0avgdata 22408maxresident)k
+librose-object-perl 2.30user 0.41system 0:02.75elapsed 98%CPU (0avgtext+0avgdata 22608maxresident)k
+nagios-images 3.03user 0.53system 0:03.69elapsed 96%CPU (0avgtext+0avgdata 86824maxresident)k
+libclass-gomor-perl 2.13user 0.36system 0:02.51elapsed 99%CPU (0avgtext+0avgdata 22640maxresident)k
+libhtml-encoding-perl 1.95user 0.35system 0:02.32elapsed 99%CPU (0avgtext+0avgdata 23220maxresident)k
+lunch 2.64user 0.48system 0:03.27elapsed 95%CPU (0avgtext+0avgdata 30928maxresident)k
+fonts-lohit-taml 1.34user 0.32system 0:01.68elapsed 99%CPU (0avgtext+0avgdata 26692maxresident)k
+libnet-ipv6addr-perl 1.81user 0.31system 0:02.16elapsed 98%CPU (0avgtext+0avgdata 22428maxresident)k
+libpetal-perl 6.43user 0.98system 0:07.44elapsed 99%CPU (0avgtext+0avgdata 22604maxresident)k
+libpoe-filter-stomp-perl 2.15user 0.40system 0:02.66elapsed 96%CPU (0avgtext+0avgdata 22580maxresident)k
+when 1.54user 0.27system 0:01.85elapsed 97%CPU (0avgtext+0avgdata 22564maxresident)k
+r-cran-rocr 3.29user 0.58system 0:03.90elapsed 99%CPU (0avgtext+0avgdata 73604maxresident)k
+libmath-base36-perl 1.98user 0.33system 0:02.33elapsed 99%CPU (0avgtext+0avgdata 22628maxresident)k
+m17n-docs 4.48user 1.30system 0:06.96elapsed 83%CPU (0avgtext+0avgdata 64084maxresident)k
+flufl.enum 5.56user 0.94system 0:06.60elapsed 98%CPU (0avgtext+0avgdata 63480maxresident)k
+metainit 1.57user 0.25system 0:01.88elapsed 96%CPU (0avgtext+0avgdata 22420maxresident)k
+aspell-el 2.01user 0.29system 0:02.32elapsed 99%CPU (0avgtext+0avgdata 23616maxresident)k
+tor-arm 3.56user 0.64system 0:04.24elapsed 99%CPU (0avgtext+0avgdata 26880maxresident)k
+nautilus-scripts-manager 2.67user 0.44system 0:03.17elapsed 98%CPU (0avgtext+0avgdata 22868maxresident)k
+log4cpp-doc 3.27user 0.71system 0:04.13elapsed 96%CPU (0avgtext+0avgdata 79776maxresident)k
+libnet-subnets-perl 1.90user 0.27system 0:02.21elapsed 98%CPU (0avgtext+0avgdata 22596maxresident)k
+libpod-simple-wiki-perl 4.21user 0.67system 0:04.94elapsed 98%CPU (0avgtext+0avgdata 22284maxresident)k
+flask 22.30user 1.39system 0:25.10elapsed 94%CPU (0avgtext+0avgdata 144004maxresident)k
+django-notification 3.94user 0.70system 0:04.72elapsed 98%CPU (0avgtext+0avgdata 26796maxresident)k
+fts 3.81user 0.87system 0:04.82elapsed 97%CPU (0avgtext+0avgdata 23020maxresident)k"""
+
+import re
+
+rrTimes = {}
+nativeTimes = {}
+
+elapsedTime = re.compile(r'(\S+).*(\d+):(\d+.\d+)elapsed')
+for l in rrRawTimeOutput.split('\n'):
+    m = elapsedTime.search(l)
+    pkg = m.group(1)
+    seconds = int(m.group(2))*60 + float(m.group(3))
+    #print(pkg, seconds)
+    rrTimes[pkg] = seconds
+    pass
+
+for l in nativeRawTimeOutput.split('\n'):
+    m = elapsedTime.search(l)
+    pkg = m.group(1)
+    seconds = int(m.group(2))*60 + float(m.group(3))
+    #print(pkg, seconds)
+    nativeTimes[pkg] = seconds
+    pass
+
+assert set(rrTimes.keys()) == set(nativeTimes.keys())
+
+print("average runtime overhead",sum(rrTimes.values()) / sum(nativeTimes.values()))
+
+slowdowns = []
+for pkg in rrTimes.keys():
+    slowdowns.append( (pkg,rrTimes[pkg] / nativeTimes[pkg]) )
+    pass
+
+print( sorted(slowdowns, key=lambda a:a[1]) )
